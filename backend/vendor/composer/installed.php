375157050786acfd1555046867e9833cf832da44
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2026-07-14 22:39:48 -0300

    atualizado gitignore

--- backend/vendor/composer/installed.php
+++ backend/vendor/composer/installed.php
@@ -28,6 +28,15 @@
             'aliases' => array(),
             'dev_requirement' => false,
         ),
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v7.1.0',
+            'version' => '7.1.0.0',
+            'reference' => 'b374a5d1a4f1f67fadc2165cdb284645945e2fc0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
         'graham-campbell/result-type' => array(
             'pretty_version' => 'v1.1.4',
             'version' => '1.1.4.0',
